7ae2e5ac0f6f0ab64a5d32dba96e15998d7bb37f
Djordje PEtrovic <petrovicdjordje750@gmail.com>
2018-09-09 13:17:22 +0200

    small corrections

--- helpers.c
+++ helpers.c
@@ -1,20 +1,11 @@
 
 #include "helpers.h"
 
-
+// <inherited.doc/>
 int getln_len(long long n)
 {
+    if(n == 0)
+    return 1;
+    
     return (int) log10(n) + 1;
-}
-
-int getln_len1(long long n)
-{
-    int counter = 0;
-    long long devider = 1;
-    while(n / devider != 0)
-    {
-        devider *= 10;
-        counter ++;
-    }
-    return counter;
 }
--- helpers.h
+++ helpers.h
@@ -2,8 +2,10 @@
 #include <math.h>
 #include <stdio.h>
 
-
+/**
+ * Calculates lenght of number type long long
+ * <returns> lenght as intiger </returns>
+ */
 int getln_len(long long n);
 
-int getln_len1(long long n);
 
